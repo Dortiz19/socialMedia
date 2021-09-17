56e52d544565431c7baee9947c079c725ab21560
Diego <dxortiz2@gmail.com>
2021-09-17 11:49:57 -0500

    skeleton complete

--- server/express.js
+++ server/express.js
@@ -7,23 +7,67 @@ import helmet from 'helmet';
 import Template from './../template';
 import userRoutes from './routes/user.routes';
 import authRoutes from './routes/auth.routes';
+import path from 'path';
+// VV commented out for production VV
+import devBundle from './devBundle';
+
+// React modules required for rendering components and use renderToString
+import React from 'react';
+import ReactDOMServer from 'react-dom/server';
+
+// Router modules
+import { StaticRouter } from 'react-router-dom'
+import MainRouter from './../client/MainRouter';
+
+// CSS modules
+import { ServerStyleSheets, ThemeProvider } from '@material-ui/styles';
+import theme from './../client/theme';
 
 
+const CURRENT_WORKING_DIR = process.cwd();
 const app = express()
-/*...configure express...*/
-app.use(bodyParser.json())
-    .use(bodyParser.urlencoded({ extended: true }))
-    .use(cookieParser())
-    .use(compress())
-    .use(helmet())
-    .use(cors());
 
-app.get('/', (req, res) => {
-    res.status(200).send(Template())
-});
+
+// VV commented out for production VV
+devBundle.compile(app);
+
+
+// parse body params and attache them to req.body
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cookieParser())
+app.use(compress())
+// secure apps by setting various HTTP headers
+app.use(helmet())
+// enable CORS - Cross Origin Resource Sharing
+app.use(cors())
+
+app.use('/dist', express.static(path.join(CURRENT_WORKING_DIR, 'dist')))
 
 app.use('/', userRoutes)
 app.use('/', authRoutes)
+
+app.get('*', (req, res) => {
+  const sheets = new ServerStyleSheets()
+  const context = {}
+  const markup = ReactDOMServer.renderToString(
+    sheets.collect(
+      <StaticRouter location={req.url} context={context}>
+        <ThemeProvider theme={theme}>
+          <MainRouter />
+        </ThemeProvider>
+      </StaticRouter>
+    )
+  )
+  if(context.url) {
+    return res.redirect(303, context.url)
+  }
+  const css = sheets.toString()
+  res.status(200).send(Template({
+    markup,
+    css
+  }))
+})
 
 app.use((err, req, res, next) => {
     if (err.name === 'UnauthorizedError') {
@@ -33,5 +77,6 @@ app.use((err, req, res, next) => {
         console.log(err)
     }
 });
+
 
 export default app;
