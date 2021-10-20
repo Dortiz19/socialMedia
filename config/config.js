4fd8bf6095b39c898d4c4ed0f36fab344ccc9f92
Diego Ortiz <74843272+Dortiz19@users.noreply.github.com>
2021-10-19 19:28:26 -0500

    Update config.js

--- config/config.js
+++ config/config.js
@@ -3,8 +3,7 @@ const config = {
     port: process.env.PORT || 3000,
     jwtSecret: process.env.JWT_SECRET || "987",
     mongoUri: process.env.MONGODB_URI ||
-      process.env.MONGO_HOST ||
-      'mongodb+srv://diego1234:diego1234@cluster0.frgvx.mongodb.net/reactnode?authSource=admin&replicaSet=atlas-1iy69z-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true'
+      process.env.MONGO_HOST
   }
   
   export default config;
